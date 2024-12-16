2248d31d80c1fc8ccc3c160fc13f4e6dd7d797d0
ritaurbani <ritaurbani@gmail.com>
2024-12-16 16:32:07 +0100

    feat: rendering card attempts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-
+import {useState} from "react"
+import languages from "../assets/languages"
 
 function App() {
-  
+
+ 
+
+  const [activeLanguage, setActiveLanguage] = useState(languages[0])
 
   return (
     <>
-     
+    <h2 className="mb-5">Learn Web Development</h2>
+     <div className="container">
+      <div>
+      {
+        languages.map((language) =>(
+          <button key ={language.id} className="btn btn-primary m-2" onClick={() => {setActiveLanguage(language)}}>{language.title}</button>      
+        ))
+      }
+      </div>
+
+      <div className="card">
+      
+      </div>
+     </div>
     </>
   )
 }
